a0d84fdb49829a24249830d05a85c90811e44d32
Max Hanrahan <43153157+max-hanrahan@users.noreply.github.com>
2020-08-07 15:51:45 -0400

    Fixed the sliders to update the data correctly

--- COVID Models/SIRD_interactive.py
+++ COVID Models/SIRD_interactive.py
@@ -1,42 +1,34 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-'''This is a simple model for tracking pandemics. 
+'''
+This is a simple model for tracking pandemics. 
 It is called the SIRD model because it tracks the number of people over time in each of four categories: 
 Susceptible, Infected, Removed/Recovered, and Deceased.
 
 This file contains interactive plots! The variables beta, gamma, and mu can be affected by sliders. 
-It is meant to be copied and pasted as plain text into the Anaconda ipython terminal after importing pylab 
-(type "ipyton -pylab" in the Anaconda shell).
+It is meant to be copied and pasted as plain text into the ipython terminal.
 '''
-
 # In[1]:
-
 
 import numpy as np 
 import math
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 
-
 # In[2]:
-
 
 beta = .09 # infection rate
 gamma = .01 # rate of recovery
 mu = .005 # mortality rate
 
-
 # In[3]:
-
 
 dt = .1 # the size of a step
 steps = np.arange(0, 3650) # 365 days in a year, one notch is a tenth of a day
 time = dt* steps # should be ints from 0 to 365
 
-
 # In[4]:
-
 
 def SIRD(s, i, r, d, beta, gamma, mu):
     # todo: document how these equations came to be
@@ -46,9 +38,7 @@ def SIRD(s, i, r, d, beta, gamma, mu):
     d_dot = mu*i 
     return s_dot, i_dot, r_dot, d_dot
 
-
 # In[5]:
-
 
 # initialize s, i, r, d as empty lists...
 s = np.empty(len(time))
@@ -62,9 +52,7 @@ i[0] = 1
 r[0] = 0
 d[0] = 0
 
-
 # In[6]:
-
 
 # simple euler's method of solving diff eq's
 def euler(s,i,r,d,beta,gamma,mu,steps,dt):
@@ -77,23 +65,20 @@ def euler(s,i,r,d,beta,gamma,mu,steps,dt):
             d[t+1] = d[t]+dt*d_dot
     return s, i, r, d
 
-
 # In[7]:
 
-
 s, i, r, d = euler(s, i, r, d, beta, gamma, mu, steps, dt)
-
 
 '''
 My choice of naming variables has been sort of convoluted, so allow me to explain:
 When s, i, r, and d were initialized, they were just lists. Then we added their initial values at t = 0. 
 Then I defined a function (called euler) which would take four numpy arrays and some adjustable paramters, 
 iteratively perform euler's method, and return those four (now updated) arrays. 
-The line directly above this comment sets the list s, i, r, d (each of which hitherto contains only one nontrivial
- element) as now equal to the result of that euler function.
+The line directly above this comment sets the list s, i, r, d (each of which hitherto contains only one nontrivial element) 
+as now equal to the result of that euler function.
 '''
-# In[8]:
 
+# In[8]:
 
 # now on to the fancy plotting:
 fig = plt.figure(figsize=(10, 6))
@@ -110,6 +95,7 @@ int_plotd, = plt.plot(time,d,markersize=.8,c='k')
 plt.legend(['Hospital Capacity', 'Infected', 'Deceased'])
 plt.tight_layout(pad = 5)
 
+# three axes, three sliders
 a_ax = plt.axes([0.12, 0.08, 0.6, 0.02])
 a_slider = Slider(a_ax,      
                   'Infection rate',            
@@ -133,26 +119,26 @@ c_slider = Slider(c_ax,
                   valinit=.005 
                  )
 slidelist = [.09,.02,.005]
-# sidelist serves as a list of beta, gamma, and mu that will be updated
+# slidelist serves as a list of beta, gamma, and mu that will be updated
 # so as not to interfere with the global variables
 
 # i must introduce some new updater functions for interaction's sake:
 def update_a(a):
-    new_data = euler(s, i, r, d, slidelist[0], gamma, mu, steps, dt)
+    new_data = euler(s, i, r, d, a, slidelist[1], slidelist[2], steps, dt)
     int_ploti.set_ydata(new_data[1])
     int_plotd.set_ydata(new_data[3])
     slidelist[0] = a
     fig.canvas.draw_idle()
     
 def update_b(b):
-    new_data = euler(s, i, r, d, beta, slidelist[1], mu, steps, dt)
+    new_data = euler(s, i, r, d, slidelist[0], b, slidelist[2], steps, dt)
     int_ploti.set_ydata(new_data[1])
     int_plotd.set_ydata(new_data[3])
     slidelist[1] = b
     fig.canvas.draw_idle()
 
 def update_c(c):
-    new_data = euler(s, i, r, d, beta, gamma, slidelist[2], steps, dt)
+    new_data = euler(s, i, r, d, slidelist[0], slidelist[1], c, steps, dt)
     int_ploti.set_ydata(new_data[1])
     int_plotd.set_ydata(new_data[3])
     slidelist[2] = c
@@ -162,3 +148,4 @@ def update_c(c):
 a_slider.on_changed(update_a)
 b_slider.on_changed(update_b)
 c_slider.on_changed(update_c)
+
